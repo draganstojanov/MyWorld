7607ca957bda3e19172670622bb7d18a035eee55
Dragan <dragan.stojanov@gmail.com>
2021-04-22 20:37:35 +0200

    network module refactor

--- app/src/main/java/com/andraganoid/myworld/di/Modules.kt
+++ app/src/main/java/com/andraganoid/myworld/di/Modules.kt
@@ -1,14 +1,24 @@
 package com.andraganoid.myworld.di
 
 
+import com.andraganoid.myworld.BuildConfig
+import com.andraganoid.myworld.api.CountriesApi
 import com.andraganoid.myworld.api.CountriesNetworkService
 import com.andraganoid.myworld.countries.CountriesViewModel
 import com.andraganoid.myworld.country.borders.BordersViewModel
 import com.andraganoid.myworld.repo.CountriesRepository
+import com.andraganoid.myworld.utils.BASE_URL
 import com.andraganoid.myworld.utils.Preferences
+import com.google.gson.Gson
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object Modules {
 
@@ -22,9 +32,34 @@ object Modules {
         single { Preferences(androidContext()) }
     }
 
-
     private val networkModule = module {
-        single { CountriesNetworkService().countriesService() }
+
+        single { Gson() }
+
+        single(override = true) {
+            val builder = OkHttpClient.Builder()
+                .readTimeout(30, TimeUnit.SECONDS)
+                .connectTimeout(30, TimeUnit.SECONDS)
+            if (BuildConfig.DEBUG) {
+                builder.addInterceptor(HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                })
+            }
+            builder.build()
+        }
+
+        single {
+            Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(get())
+                .addConverterFactory(GsonConverterFactory.create(get()))
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .build()
+        }
+
+        single<CountriesApi> {
+            get<Retrofit>().create(CountriesApi::class.java)
+        }
     }
 
     val appModule =
@@ -36,5 +71,6 @@ object Modules {
 
 
 }
+
 
 
